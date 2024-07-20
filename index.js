b61d1b5a07b1df5493f30fcc4bb69d1879eaa7a9
Tanmay Jain <115374931+Tanmay-312@users.noreply.github.com>
2024-07-20 17:47:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cookiesParser = require('cookie-parser')
 const { app, server } = require('./socket/index.js')
 
 // middleware
-//app.use(express.json())
+app.use(express.json())
 app.use(cors());
 app.use(cookiesParser())
 
