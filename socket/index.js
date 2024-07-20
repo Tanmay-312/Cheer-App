e8f3c537c6edecad50ef3dbcf099e52b0433c314
Tanmay Jain <115374931+Tanmay-312@users.noreply.github.com>
2024-07-20 18:11:25 +0530

    Update index.js

--- socket/index.js
+++ socket/index.js
@@ -12,7 +12,6 @@ const app = express()
 const server = http.createServer(app)
 const io = new Server(server,{
     cors : {
-        origin: 'https://cheer-app.vercel.app',
         credentials : true
     }
 })
