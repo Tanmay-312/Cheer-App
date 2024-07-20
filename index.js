49f63f63256899f65f5dc4d9ce8b4d6f6234caa7
Tanmay Jain <115374931+Tanmay-312@users.noreply.github.com>
2024-07-20 17:39:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,8 @@ const router = require('./routes/index.js')
 const cookiesParser = require('cookie-parser')
 const { app, server } = require('./socket/index.js')
 
-const app = express()
-const port = process.env.PORT || 8080
-
 // middleware
-app.use(express.json())
+//app.use(express.json())
 app.use(cors());
 app.use(cookiesParser())
 
